f6ca0850e7bbf570094e4a63f8ea4d27a3ad447f
zyenapz <zyenapz@gmail.com>
2020-12-05 15:00:48 +0800

    Updated hp bar

--- scripts/draw.py
+++ scripts/draw.py
@@ -23,7 +23,7 @@ def draw_hp(surf, x, y, hp, color, img):
     if hp < 0:
         hp = 0
     rect_height = img.get_height()*0.5
-    rectangle = pygame.Rect(x+24, y+8,  hp * 10, rect_height)
+    rectangle = pygame.Rect(x+24, y+8,  hp * 23, rect_height)
     pygame.draw.rect(surf, color, rectangle)
     surf.blit(img, (x, y))
 
