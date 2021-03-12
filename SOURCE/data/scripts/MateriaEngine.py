03c3eedd623e5c181bcef3db9b8d0caa7d5933cb
zyenapz <zyenapz@gmail.com>
2021-03-12 18:23:35 +0800

    Added MateriaEngine for future updates

---
+++ SOURCE/data/scripts/MateriaEngine.py
@@ -0,0 +1,55 @@
+# Materia Engine
+# A framework built on top of pygame to make development of games easier.
+# Author: zyenapz
+
+import pygame
+
+# CLASSES === === ===
+
+class Scene():
+    def __init__(self):
+        pass
+    
+    def handle_events(self, events):
+        raise NotImplementedError
+    
+    def update(self):
+        raise NotImplementedError
+
+    def draw(self, window):
+        raise NotImplementedError
+
+class SceneManager(object):
+    def __init__(self, InitScene):
+        self.go_to(InitScene)
+
+    def go_to(self, scene):
+        self.scene = scene
+        self.scene.manager = self
+
+# FUNCTIONS === === ===
+
+def load_sound(filename, sfx_dir, volume):
+    path = os.path.join(sfx_dir, filename)
+    snd = pygame.mixer.Sound(path)
+    snd.set_volume(volume)
+    return snd
+
+def load_img(file, directory, scale, convert_alpha=False):
+    try:
+        path = os.path.join(directory, file)
+        if not convert_alpha:
+            img = pygame.image.load(path).convert_alpha()
+        else:
+            img = pygame.image.load(path).convert()
+            transColor = img.get_at((0,0))
+            img.set_colorkey(transColor)
+        img_w = img.get_width()
+        img_h = img.get_height()
+        img = pygame.transform.scale(img, (img_w*scale, img_h*scale))
+        return img
+    except Exception as e:
+        print(f"Error for {file}: {e} Loading default texture instead.")
+        s = pygame.Surface((32,32))
+        s.fill('red')
+        return s
