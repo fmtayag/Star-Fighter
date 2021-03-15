25fc1e4e8776bed7ff4dc65e978a7a62f613d77b
zyenapz <zyenapz@gmail.com>
2021-03-15 18:42:24 +0800

    restructured codebase

---
+++ SOURCE/data/scripts/scenes.py
@@ -0,0 +1,350 @@
+import pygame
+from data.scripts.settings import *
+from data.scripts.MateriaEngine import (
+    load_img, 
+    load_sound, 
+    sort,
+    read_savedata,
+    write_savedata,
+    Scene,
+    SceneManager,
+    draw_background, 
+    draw_text,
+    shake,
+    slice_list
+)
+
+# TITLE SCENE ================================
+
+class TitleMenu:
+    def __init__(self, init_selected):
+        # Surface
+        # Warning - options may go beyond the surface and will be not rendered
+        self.surface = pygame.Surface((WIN_RES["w"], 350))
+        self.surf_rect = self.surface.get_rect()
+        
+        # Text settings
+        self.font_size = 32
+        self.spacing = self.font_size / 2
+
+        # Menu
+        self.options = ("PLAY", "SCORES", "OPTIONS", "CREDITS", "EXIT")
+        self.act_opt = [0 for i in range(len(self.options))] # Active options
+        self.act_opt[init_selected] = 1
+        self.colors = {0: "white", 1: "black"} # Colors for active/inactive menu
+
+        # Selector
+        self.selector = pygame.Surface((WIN_RES["w"], self.font_size + 4))
+        self.selector.fill("white")
+        self.sel_y = self.font_size + self.spacing
+        self.sel_i = init_selected # index
+
+    def update(self):
+        self.sel_y = self.font_size*(self.sel_i+1) + self.spacing*(self.sel_i+1)
+
+    def draw(self, window):
+        self.surface.fill("black")
+        self.surface.set_colorkey("black")
+        self.surface.blit(self.selector, (0,self.sel_y-3))
+        for i in range(len(self.options)):
+            draw_text(self.surface, self.options[i], self.font_size, GAME_FONT, self.surf_rect.centerx, self.font_size*(i+1) + self.spacing*(i+1), self.colors[self.act_opt[i]], "centered")
+        window.blit(self.surface, (0,window.get_height()/2 - 80))
+
+    def select_up(self):
+        if  self.sel_i > 0:
+            self.act_opt[self.sel_i] = 0
+            self.sel_i -= 1
+            self.act_opt[self.sel_i] = 1
+        else:
+            self.act_opt[self.sel_i] = 0
+            self.sel_i = len(self.options) - 1
+            self.act_opt[self.sel_i] = 1
+
+    def select_down(self):
+        if self.sel_i < len(self.options) - 1:
+            self.act_opt[self.sel_i] = 0
+            self.sel_i += 1
+            self.act_opt[self.sel_i] = 1
+        else:
+            self.act_opt[self.sel_i] = 0
+            self.sel_i = 0
+            self.act_opt[self.sel_i] = 1
+
+    def get_selected(self):
+        return self.options[self.sel_i]
+
+class TitleScene(Scene):
+    def __init__(self, init_selected):
+        # Text settings
+        self.font_size = 32
+
+        # Background
+        self.bg_img = load_img("background.png", IMG_DIR, SCALE)
+        self.bg_rect = self.bg_img.get_rect()
+        self.bg_y = 0
+        self.par_img = load_img("background_parallax.png", IMG_DIR, SCALE)
+        self.par_rect = self.bg_img.get_rect()
+        self.par_y = 0
+
+        # Images
+        self.logo_img = load_img("logo.png", IMG_DIR, 8, convert_alpha=True)
+
+        # Sounds
+        self.select_sfx = load_sound("sfx_select.wav", SFX_DIR, 0.5)
+
+        # Menu object
+        self.title_menu = TitleMenu(init_selected)
+
+    def handle_events(self, events):
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    self.title_menu.select_up()
+                    self.select_sfx.play()
+                elif event.key == pygame.K_DOWN:
+                    self.title_menu.select_down()
+                    self.select_sfx.play()
+                elif event.key == pygame.K_z:
+                    if self.title_menu.get_selected() == "PLAY":
+                        print("TODO - PLAY")
+                    elif self.title_menu.get_selected() == "SCORES":
+                        self.manager.go_to(ScoresScene())
+                    elif self.title_menu.get_selected() == "OPTIONS":
+                        print("TODO - OPTIONS")
+                    elif self.title_menu.get_selected() == "CREDITS":
+                        print("TODO - CREDITS")
+                    elif self.title_menu.get_selected() == "EXIT":
+                        print("TODO - EXIT")
+
+    def update(self, dt):
+        self.bg_y += 100 * dt
+        self.par_y += 200 * dt
+        self.title_menu.update()
+
+    def draw(self, window):
+        draw_background(window, self.bg_img, self.bg_rect, self.bg_y)
+        draw_background(window, self.par_img, self.par_rect, self.par_y)
+        window.blit(self.logo_img, (window.get_width()/2-240, -128))
+
+        # Draw menu
+        self.title_menu.draw(window)
+        draw_text(window, "Actually...there's no copyright", int(self.font_size/2), GAME_FONT, window.get_rect().centerx, window.get_rect().bottom-(self.font_size/2)*2, "WHITE", "centered")
+
+# SCORES SCENE ===============================
+
+class ScoresControlPanel():
+    def __init__(self):
+        # Text settings
+        self.font_size = 32
+
+        # Panels
+        self.sub_panels = ("DIRECTION", "BACK")
+        self.active_panel = self.sub_panels[0]
+        self.colors = {0: "white", 1: "black"} # Colors for active/inactive options
+
+        # Direction Sub-panel
+        self.direction_panel = pygame.Surface((WIN_RES["w"], 64))
+        self.dp_rect = self.direction_panel.get_rect()
+
+        # Direction Sub-panel Options
+        self.dp_options = ("PREV", "NEXT")
+        self.dp_act_opt = [0 for i in range(len(self.dp_options))] # Active options
+        self.dp_act_opt[0] = 1
+
+        # Back Sub-panel
+        self.back_panel = pygame.Surface((WIN_RES["w"], 64))
+        self.bp_rect = self.back_panel.get_rect()
+
+        # Back Sub-panel Option
+        self.bp_options = ("BACK")
+        self.bp_active = 0
+
+        # Selector
+        self.selector = pygame.Surface((self.dp_rect.width*0.5, self.dp_rect.height))
+        self.selector.fill("WHITE")
+        self.sel_i = 0 # index
+
+    def update(self):
+        pass
+
+    def draw(self, window):
+        # Set colorkeys
+        self.direction_panel.fill("BLACK")
+        self.direction_panel.set_colorkey("BLACK")
+        self.back_panel.fill("BLACK")
+        self.back_panel.set_colorkey("BLACK")
+
+        # Selector
+        if self.active_panel == self.sub_panels[0]:
+            self.direction_panel.blit(self.selector, (self.dp_rect.centerx*self.sel_i,0))
+        elif self.active_panel == self.sub_panels[1]:
+            self.back_panel.blit(self.selector, (self.back_panel.get_rect().width/4,0))
+
+        # Direction panel
+        draw_text(self.direction_panel, self.dp_options[0], self.font_size, GAME_FONT, self.dp_rect.centerx*0.5, self.dp_rect.centery*0.5, self.colors[self.dp_act_opt[0]], "centered")
+        draw_text(self.direction_panel, self.dp_options[1], self.font_size, GAME_FONT, self.dp_rect.centerx*1.5, self.dp_rect.centery*0.5, self.colors[self.dp_act_opt[1]], "centered")
+        window.blit(self.direction_panel, (0,window.get_rect().height*0.7))
+
+        # Back panel
+        draw_text(self.back_panel, "BACK", self.font_size, GAME_FONT, self.dp_rect.centerx, self.dp_rect.centery*0.5, self.colors[self.bp_active], "centered")
+        window.blit(self.back_panel, (0,window.get_rect().height*0.8))
+
+    def move_left(self):
+        if self.active_panel == self.sub_panels[0]:
+            if self.sel_i > 0:
+                self.dp_act_opt[self.sel_i] = 0
+                self.sel_i -= 1
+                self.dp_act_opt[self.sel_i] = 1
+
+        elif self.active_panel == self.sub_panels[1]:
+            self.active_panel = self.sub_panels[0]
+            self.sel_i = 0
+            self.move_up()
+
+    def move_right(self):
+        if self.active_panel == self.sub_panels[0]:
+            if self.sel_i < len(self.dp_options)-1:
+                self.dp_act_opt[self.sel_i] = 0
+                self.sel_i += 1
+                self.dp_act_opt[self.sel_i] = 1
+
+        elif self.active_panel == self.sub_panels[1]:
+            self.active_panel = self.sub_panels[0]
+            self.sel_i = len(self.dp_options)-1
+            self.move_up()
+
+    def move_up(self):
+        self.active_panel = self.sub_panels[0]
+        self.bp_active = 0
+
+        self.dp_act_opt = [0 for i in range(len(self.dp_options))] # Active options
+        self.dp_act_opt[self.sel_i] = 1
+
+    def move_down(self):
+        self.active_panel = self.sub_panels[1]
+        self.bp_active = 1
+
+        self.dp_act_opt = [0 for i in range(len(self.dp_options))] # Active options
+        self.dp_act_opt[0] = 0
+
+    def get_active_panel(self):
+        return self.active_panel
+
+    def get_dp_selected_option(self):
+        if self.active_panel == self.sub_panels[0]:
+            return self.dp_options[self.sel_i]
+
+class ScoresTable():
+    def __init__(self):
+        # Text settings
+        self.font_size = 32
+        self.spacing = self.font_size / 2
+
+        # Table
+        self.table_surf = pygame.Surface((WIN_RES["w"], WIN_RES["h"] / 2))
+        self.table_rect = self.table_surf.get_rect()
+        #self.table_surf.fill('red')
+        
+        # Scores - TODO: Just an example. No sorting yet.
+        self.scores = [
+            ["MAN", 1000],
+            ["WMN", 2100],
+            ["LOL", 102],
+            ["XMS", 106],
+            ["EBL", 107],
+            ["LOL", 102],
+            ["XMS", 106],
+            ["EBL", 107],
+            ["LOL", 102],
+            ["XMS", 106],
+            ["EBL", 107],
+            ["LOL", 102],
+            ["XMS", 106],
+            ["LOL", 102],
+            ["XMS", 106],
+            ["EBL", 107],
+            ["LOL", 102],
+            ["XMS", 106]
+        ]
+        self.splice_n = 5
+        self.scores = slice_list(self.scores, self.splice_n)
+        self.cur_tbl = 0
+
+    def update(self):
+        pass
+
+    def draw(self, window):
+        self.table_surf.fill("black")
+        self.table_surf.set_colorkey("black")
+
+        for i in range(len(self.scores[self.cur_tbl])):
+            draw_text(self.table_surf, f"{(i+1)+(self.cur_tbl*self.splice_n)}.", self.font_size, GAME_FONT, self.table_rect.centerx * 0.5 + len(str(i)), self.font_size*(i+1) + self.spacing*(i+1), "YELLOW")
+            draw_text(self.table_surf, f"{self.scores[self.cur_tbl][i][0]}", self.font_size, GAME_FONT, self.table_rect.centerx * 0.8, self.font_size*(i+1) + self.spacing*(i+1), "WHITE")
+            draw_text(self.table_surf, f"{self.scores[self.cur_tbl][i][1]}", self.font_size, GAME_FONT, self.table_rect.centerx * 1.3, self.font_size*(i+1) + self.spacing*(i+1), "WHITE")
+
+        # TODO - this is just a placeholder
+        draw_text(window, f"PAGE {self.cur_tbl+1} OF {len(self.scores)}", self.font_size, GAME_FONT, self.table_rect.centerx, self.table_rect.bottom * 1.25, "WHITE", "centered")
+
+        window.blit(self.table_surf,(0,WIN_RES["h"]/2 - 256))
+
+    def next_table(self):
+        if self.cur_tbl < len(self.scores) - 1:
+            self.cur_tbl += 1
+        
+    def prev_table(self):
+        if self.cur_tbl > 0:
+            self.cur_tbl -= 1
+
+class ScoresScene(Scene):
+    def __init__(self):
+        # Background
+        self.bg_img = load_img("background.png", IMG_DIR, SCALE)
+        self.bg_rect = self.bg_img.get_rect()
+        self.bg_y = 0
+        self.par_img = load_img("background_parallax.png", IMG_DIR, SCALE)
+        self.par_rect = self.bg_img.get_rect()
+        self.par_y = 0
+
+        # Sounds
+        self.select_sfx = load_sound("sfx_select.wav", SFX_DIR, 0.5)
+
+        # Scores table
+        self.scores_table = ScoresTable()
+
+        # Control panel
+        self.control_panel = ScoresControlPanel()
+    
+    def handle_events(self, events):
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    self.control_panel.move_left()
+                elif event.key == pygame.K_RIGHT:
+                    self.control_panel.move_right()
+                elif event.key == pygame.K_UP:
+                    self.control_panel.move_up()
+                elif event.key == pygame.K_DOWN:
+                    self.control_panel.move_down()
+
+                elif event.key == pygame.K_z:
+                    if self.control_panel.get_active_panel() == "DIRECTION":
+                        if self.control_panel.get_dp_selected_option() == "PREV":
+                            self.scores_table.prev_table()
+                        elif self.control_panel.get_dp_selected_option() == "NEXT":
+                            self.scores_table.next_table()
+                    elif self.control_panel.get_active_panel() == "BACK":
+                        self.manager.go_to(TitleScene(1))
+                elif event.key == pygame.K_x:
+                    self.manager.go_to(TitleScene(1))
+    
+    def update(self, dt):
+        self.bg_y += 100 * dt
+        self.par_y += 200 * dt
+
+    def draw(self, window):
+        draw_background(window, self.bg_img, self.bg_rect, self.bg_y)
+        draw_background(window, self.par_img, self.par_rect, self.par_y)
+
+        draw_text(window, "HALL OF FAME", 64, GAME_FONT, window.get_rect().centerx, 64, "WHITE", "centered")
+        self.scores_table.draw(window)
+        self.control_panel.draw(window)
--- SOURCE/data/scripts/settings.py
+++ SOURCE/data/scripts/settings.py
@@ -1,4 +1,25 @@
+import os
+
+# Colors
+BLACK = (0,0,0)
+WHITE = (235,235,235)
+GRAY = (100,100,100)
+RED = (180,32,42)
+GOLD = (255,215,0)
+PURPLE = (219,84,180)
+
+# Window Resolution and Metadata
 WIN_RES = {"w": 640, "h": 780}
 TITLE = "Star Fighter"
 AUTHOR = "zyenapz"
-VERSION = "1.0"
+VERSION = "2.0"
+SCALE = 4
+
+# Directories
+GAME_DIR = os.path.dirname("..") # What the fuck?
+DATA_DIR = os.path.join(GAME_DIR, "data")
+FONT_DIR = os.path.join(DATA_DIR, "font")
+IMG_DIR = os.path.join(DATA_DIR, "img")
+SCRIPTS_DIR = os.path.join(DATA_DIR, "scripts")
+SFX_DIR = os.path.join(DATA_DIR, "sfx")
+GAME_FONT = os.path.join(FONT_DIR, "04B_03__.TTF")
