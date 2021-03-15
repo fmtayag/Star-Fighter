3c470707191793dfc1bc04d3e713878dd586ffec
zyenapz <zyenapz@gmail.com>
2021-03-15 19:41:35 +0800

    added pygame version
    it's 2.0.0

--- SOURCE/game.py
+++ SOURCE/game.py
@@ -2,6 +2,7 @@
 # Version: 2.0.0
 #   > Release date: March 30th 2021
 # A space shoot 'em up (shmup) made in the style of old arcade games.
+# Written in: Python, pygame 2.0.0
 # Author: zyenapz
 #   > Email: zyenapz@gmail.com
 #   > Website: zyenapz.github.io
