d2724f1b68163c81de9879a0dc40d7d2f4f2368b
zyenapz <zyenapz@gmail.com>
2021-04-24 07:29:38 +0800

    tweaked the background

--- SOURCE/data/scripts/m_PlayerPrefs.py
+++ SOURCE/data/scripts/m_PlayerPrefs.py
@@ -2,9 +2,8 @@ class PlayerPrefs:
     def __init__(self):
         self.is_fullscreen = False
         self.music_vol = 1.0
-        self.sfx_vol = 0.5
-
-        # Session settings
-        self.title_selected = 0
+        self.sfx_vol = 1.0
         self.game_difficulty = 0
-        self.score = 0
+
+        self.score = 0 # should just pass this from the game scene to the game over scene
+        self.title_selected = 0 # this too
